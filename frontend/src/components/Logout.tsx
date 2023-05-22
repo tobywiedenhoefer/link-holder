a4e6049e870e784c7f1893bd5fc53c1488ba3286
toby.wiedenhoefer <toby.wiedenh@gmail.com>
2023-05-22 07:50:01 -0500

    Added logout functionality

--- frontend/src/components/Logout.tsx
+++ frontend/src/components/Logout.tsx
@@ -1,1 +1,26 @@
-export const Logout = () => <div />
+import React, {useEffect, useState} from "react"
+import axios from "axios"
+
+export const Logout = () => {
+    useEffect(() => {
+        (async () => {
+            try {
+                const {data} = await axios.post(
+                    'http://localhost:8000', {
+                        'refresh_token': localStorage.getItem('refresh_token')
+                    },
+                    {
+                        headers: {'Content-Type': 'application/json'},
+                        withCredentials: true
+                    }
+                )
+                localStorage.clear()
+                axios.defaults.headers.common['Authorization'] = null
+                window.location.href = '/login'
+            } catch (e) {
+                console.log('logout not working', e)
+            }
+        })()
+    }, [])
+    return <div />
+}
