9fd20922c7c8168c9cc209ab9d607e2386b2bba0
toby.wiedenhoefer <toby.wiedenh@gmail.com>
2023-05-22 20:02:42 -0500

    Added mock data

--- backend/authentication/views.py
+++ backend/authentication/views.py
@@ -1,15 +1,44 @@
+from typing import List
+
 import rest_framework.status as status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.tokens import RefreshToken
 
+from .models.card import Card
+
 
 class HomeView(APIView):
     permission_classes = (IsAuthenticated, )
 
     def get(self, request) -> Response:
-        content = {'message': 'Welcome to the JWT Authentication page using React JS and Django!'}
+        auth_token = request.META['HTTP_AUTHORIZATION']
+        if not auth_token:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        cards: List[Card] = [
+            Card(
+                user_id='1',
+                card_id='1',
+                link='https://www.twitter.com',
+                description="Twitter's official website",
+                tags=['Official', 'twitter'],
+                collection=1
+            ),
+            Card(
+                user_id='1',
+                card_id='2',
+                link='https://www.google.com',
+                description="Google's official website",
+                tags=['Official', 'google'],
+                collection=1
+            ),
+        ]
+        content = {
+            'message': 'Welcome to the JWT Authentication page using React JS and Django!',
+            'cards': [card.build_dict() for card in cards]
+        }
+        print('content: ', content)
         return Response(content)
 
 
